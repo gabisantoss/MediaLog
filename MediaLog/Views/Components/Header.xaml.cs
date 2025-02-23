254874c7c11ff26088fc404847e1095a22ffa99c
Gabriela Cruz <gabrielasnts30@gmail.com>
2025-02-23 18:46:34 -0300

    add Header and search bar

---
+++ MediaLog/Views/Components/Header.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MediaLog.Views.Components
+{
+    public partial class Header : UserControl
+    {
+        public Header()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeWindow(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.WindowState = window.WindowState == WindowState.Maximized
+                                 ? WindowState.Normal
+                                 : WindowState.Maximized;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+    }
+}
